ee5bfff6e20e889059bd3108ed9339ce065c03ad
dentate.gyrus <dentategyrus13@gmail.com>
2019-03-12 12:17:44 +0000

    this.state added

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
-  render() {
-    return(
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: null,
+        };
+      }
 
-    <button
-      className="square"
-      onClick={function() {
-        alert("click");}}>
-      {this.props.value}
-    </button>);
-  }
-}
+    render() {
+      return (
+        <button className="square" onClick={() => alert('click')}>
+          {this.props.value}
+        </button>
+      );
+    }
+   }
 
 class Board extends React.Component {
   renderSquare(i) {
